f64a2e277a6628c94a7f1297b6699c6285aff55d
Steve <ycsteven.tsao@gmail.com>
2022-12-01 07:38:30 +0800

    阻擋xxs攻擊

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 
+const html = "<img onerror='alert(\"Hacked!\")' src='invalid_image'/>"; //這段會被阻擋
+const jsHacked = "javascript: alert('Hakced!')"; // 這段無法阻擋
+
 function App() {
   return (
     <ul>
