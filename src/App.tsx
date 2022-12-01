d445edd59d5dd9f3ff16f11c5d3465133424118e
Steve <ycsteven.tsao@gmail.com>
2022-12-02 07:23:56 +0800

    chore:jss module

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.css';
 import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 
@@ -7,11 +7,13 @@ const jsHacked = "javascript: alert('Hakced!')"; // 這段無法阻擋
 
 function App() {
   return (
-    <ul>
-      {robots.map((r) => (
-        <Robot id={r.id} email={r.email} name={r.name} />
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.robotList}>
+        {robots.map((r) => (
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </div>
+    </div>
   );
 }
 
