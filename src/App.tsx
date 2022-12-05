33f68a8b8d859b082c342f0804ea13e82cf3e434
Steve <ycsteven.tsao@gmail.com>
2022-12-06 07:48:23 +0800

    chore:life cycle,access api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
-import robots from './mockdata/robots.json';
+// import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart';
@@ -8,21 +8,46 @@ import ShoppingCart from './components/ShoppingCart';
 // const html = "<img onerror='alert(\"Hacked!\")' src='invalid_image'/>"; //這段會被阻擋
 // const jsHacked = "javascript: alert('Hakced!')"; // 這段無法阻擋
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} alt="logo" className={styles.appLogo} />
-        <h1>羅伯特機器人 Robot 裝甲明朝</h1>
+interface Props {}
+
+/**
+ * 來自網路請求的資源
+ */
+interface State {
+  robotGallery: any[];
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+    };
+  }
+
+  // api
+  componentDidMount(): void {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} alt="logo" className={styles.appLogo} />
+          <h1>羅伯特機器人 Robot 裝甲明朝</h1>
+        </div>
+        <ShoppingCart />
+        <div className={styles.robotList}>
+          {this.state.robotGallery.map((r) => (
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div>
       </div>
-      <ShoppingCart />
-      <div className={styles.robotList}>
-        {robots.map((r) => (
-          <Robot id={r.id} email={r.email} name={r.name} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
