5fc4c43546d8e36f14948b2b1208a85cd0469124
Steve <ycsteven.tsao@gmail.com>
2022-12-07 06:33:51 +0800

    chore:life cycle

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,10 @@ interface State {
 }
 
 class App extends React.Component<Props, State> {
+  /**
+   * 生命週期第一階段：初始化
+   * 初始化組件 state
+   */
   constructor(props) {
     super(props);
     this.state = {
@@ -27,12 +31,52 @@ class App extends React.Component<Props, State> {
     };
   }
 
-  // api
+  /**
+   * api
+   * component 建立好 dom 元素後，掛載進頁面時調用
+   */
   componentDidMount(): void {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((data) => this.setState({ robotGallery: data }));
   }
+
+  /**
+   * 生命週期第二階段：更新
+   */
+
+  // 組件接收到新的 prop (更新後) 被調用，會產生無法預測的副作用
+  // componentWillReceiveProps(
+  //   nextProps: Readonly<Props>,
+  //   nextContext: any
+  // ): void {}
+
+  // 取代，組件更新或初始化後被調用
+  // getDeriveStateFromProps(nextProps,prevState){}
+
+  // 花費較大的資源開銷，判斷何時更新畫面
+  // shouldComponentUpdate(
+  //   nextProps: Readonly<Props>,
+  //   nextState: Readonly<State>,
+  //   nextContext: any
+  // ): boolean {
+  //   return nextState.some !== this.state.some;
+  // }
+
+  // 組件更新後被調用
+  componentDidUpdate(
+    prevProps: Readonly<Props>,
+    prevState: Readonly<State>,
+    snapshot?: any
+  ): void {}
+
+  /**
+   * 生命週期第三階段：銷毀
+   * 組件銷毀後調用
+   * 可以當作 destructor 來使用
+   * 用來避免組件銷毀時可能產生的記憶體洩漏
+   */
+  componentWillUnmount(): void {}
 
   render() {
     return (
