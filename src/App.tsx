3150ab5f2a3d2f2355ec7534f4860b108ca1f98a
Steve <ycsteven.tsao@gmail.com>
2022-12-08 06:47:15 +0800

    feat:use async,await in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -27,9 +27,16 @@ const App: React.FC = (props) => {
   }, [count]);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((data) => setRobotGallery(data));
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      // .then((response) => response.json())
+      // .then((data) => setRobotGallery(data));
+      const data = await response.json();
+      setRobotGallery(data);
+    };
+    fetchData();
   }, []);
 
   return (
